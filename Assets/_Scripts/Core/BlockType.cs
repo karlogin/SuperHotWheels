ca8fce874caf6d27f8306b65cbec4c1da67ae1b2
karlog <karthik13101992@gmail.com>
2020-02-01 17:33:19 -0400

    Create script to assign block type

---
+++ Assets/_Scripts/Core/BlockType.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SuperHotWheels.Core
+{
+	public enum BlockType
+	{
+		EMPTY = -1,
+		START = 0,
+		END = 1,
+		STRAIGHT = 2,
+		CURVE = 3,
+		VERTICAL_CURVE = 4
+	}
+}
