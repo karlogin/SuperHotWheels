80e23c16192c6a7530b6c41b186f5dd2f2cadb31
karlog <karthik13101992@gmail.com>
2020-02-01 17:34:58 -0400

    Update block object script to include block type

--- Assets/_Scripts/Core/GridBlockObject.cs
+++ Assets/_Scripts/Core/GridBlockObject.cs
@@ -5,10 +5,7 @@ namespace SuperHotWheels.Core
 	[CreateAssetMenu(fileName = "NewBlock", menuName = "Super Hot Wheels/New Block", order = 1)]
 	public class GridBlockObject : ScriptableObject
 	{
-		[Header("Block Dimension (in cms)")]
-		[SerializeField] private int sizeX;
-		[SerializeField] private int sizeY;
-		[Space]
+		[SerializeField] private BlockType blockType;
 		[SerializeField] private GameObject blockAsset;
 	}
 }
