8615b500c8f6b1e16d8ec45d787f7bdce982540d
karlog <karthik13101992@gmail.com>
2020-02-01 10:47:33 -0400

    Create script to generate a empty gird

---
+++ Assets/_Scripts/Core/GridGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace SuperHotWheels.Core
+{
+	public class GridGenerator : MonoBehaviour
+	{
+		[Header("Grid Dimension (in cms)")]
+		[SerializeField] private int gridSizeX;
+		[SerializeField] private int gridSizeY;
+
+		[SerializeField] private int blockSize;
+
+		private int gridDimensionX;
+		private int gridDimensionY;
+		private int[,] grid;
+
+		private void OnEnable()
+		{
+			InitializeGrid();
+		}
+
+		private void OnDisable()
+		{
+			//
+		}
+
+		private void InitializeGrid()
+		{
+			int gridDimensionX = gridSizeX / blockSize;
+			int gridDimensionY = gridSizeY / blockSize;
+
+			grid = new int[gridDimensionX, gridDimensionY];
+
+			ClearGrid();
+		}
+
+		private void ClearGrid()
+		{
+			for (int i = 0; i < gridDimensionX; i++)
+			{
+				for (int j = 0; j < gridDimensionY; j++)
+				{
+					grid[i, j] = -1;
+				}
+			}
+		}
+	}
+}
