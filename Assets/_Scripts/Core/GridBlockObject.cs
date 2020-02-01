4aeeee81734bc8a65b802b420a8d86cb95f8bde7
karlog <karthik13101992@gmail.com>
2020-02-01 11:24:30 -0400

    Create a scriptable object script that holds the block data

---
+++ Assets/_Scripts/Core/GridBlockObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SuperHotWheels.Core
+{
+	[CreateAssetMenu(fileName = "NewBlock", menuName = "Super Hot Wheels/New Block", order = 1)]
+	public class GridBlockObject : ScriptableObject
+	{
+		[Header("Block Dimension (in cms)")]
+		[SerializeField] private int sizeX;
+		[SerializeField] private int sizeY;
+		[Space]
+		[SerializeField] private GameObject blockAsset;
+	}
+}
