dc79120c641f7725f31c32740228b32e97d26b6c
karlog <karthik13101992@gmail.com>
2020-02-01 17:32:33 -0400

    Create a script to rotate blocks

---
+++ Assets/_Scripts/Core/Block.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SuperHotWheels.Core
+{
+	public class Block : MonoBehaviour
+	{
+		[SerializeField] private float blockRotation;
+
+		private void OnValidate()
+		{
+			transform.localEulerAngles = new Vector3(0.0f, blockRotation, 0.0f);
+		}
+	}
+}
